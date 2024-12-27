4e7327dccc307bf7e2529867a8e696a4137664af
swastideep maharana <swastideep67maharana@gmail.com>
2024-12-28 01:01:48 +0530

    #added controllers,routes,login,logout,forgetpassword, registeruser,imageupload done

--- backend/config/sendEmail.js
+++ backend/config/sendEmail.js
@@ -1,28 +1,28 @@
 import { Resend } from "resend";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-if (process.env.RESEND_API) {
-  console.log("Provide RESEND_API in side the .env file");
+if (!process.env.RESEND_API) {
+  console.error("Provide RESEND_API in the .env file");
+  process.exit(1);
 }
 
 const resend = new Resend(process.env.RESEND_API);
 
 const sendEmail = async ({ sendTo, subject, html }) => {
   try {
-    const { data, error } = await resend.emails.send({
+    const data = await resend.emails.send({
       from: "Nextmart <onboarding@resend.dev>",
       to: sendTo,
       subject: subject,
       html: html,
     });
 
-    if (error) {
-      return console.error({ error });
-    }
     return data;
   } catch (error) {
-    console.log(error);
+    console.error("Failed to send email:", error);
+    throw error; // Rethrow the error for further handling if necessary
   }
 };
 
--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,14 @@ import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import helmet from "helmet";
 import connectDB from "./config/connectDB.js";
+import userRouter from "./route/user.route.js";
+import categoryRouter from "./route/category.route.js";
+import uploadRouter from "./route/upload.router.js";
+import subCategoryRouter from "./route/subCategory.route.js";
+import productRouter from "./route/product.route.js";
+import cartRouter from "./route/cart.route.js";
+import addressRouter from "./route/address.route.js";
+import orderRouter from "./route/order.route.js";
 
 const app = express();
 app.use(
@@ -19,7 +27,7 @@ app.use(cookieParser());
 app.use(morgan());
 app.use(
   helmet({
-    crossOriginOpenerPolicy: false,
+    crossOriginResourcePolicy: false,
   })
 );
 
@@ -32,8 +40,17 @@ app.get("/", (request, response) => {
   });
 });
 
+app.use("/api/user", userRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/file", uploadRouter);
+app.use("/api/subcategory", subCategoryRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/address", addressRouter);
+app.use("/api/order", orderRouter);
+
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log("Server is running", PORT);
   });
 });
