c32a6cff7d90a9507aba4072fae15bb1f7c05bf4
swastideep maharana <swastideep67maharana@gmail.com>
2024-12-30 06:26:16 +0530

    #fixing the env

--- frontend/src/common/SummaryApi.js
+++ frontend/src/common/SummaryApi.js
@@ -1,5 +1,5 @@
 // export const baseURL = import.meta.env.VITE_API_URL;
-export const baseURL = "http://localhost:8080";
+export const baseURL = import.meta.env.VITE_API_URL;
 
 const SummaryApi = {
   register: {
